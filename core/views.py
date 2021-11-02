f640a556583c3f88d96adc2120b3b45fd6a047b3
Aleksey Lovchikov <svdmusic@gmail.com>
2021-11-02 19:03:44 +0300

    Initialize project

---
+++ core/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def customer_page(request):
+    return render(request, 'home.html')
+
+
+def courier_page(request):
+    return render(request, 'home.html')
